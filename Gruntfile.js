b5a70b4486acbe68d32d71611bd5467d5275a7c8
Doug Martin <doug@dougamartin.com>
2013-06-05 21:07:53 -0500

    v0.0.6
    * wrapper optimization, now a wrapper is not created unless needed

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,5 +44,4 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-it');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-browserify');
 };
--- declare.js
+++ declare.js
@@ -476,6 +476,8 @@
     function createDeclared() {
         var arraySlice = Array.prototype.slice, classCounter = 0, Base, forceNew = new Function();
 
+        var SUPER_REGEXP = /(super)/g;
+
         function argsToArray(args, slice) {
             slice = slice || 0;
             return arraySlice.call(args, slice);
@@ -614,16 +616,37 @@
 
 
         function functionWrapper(f, name) {
-            var wrapper = function wrapper() {
-                var ret, meta = this.__meta || {};
-                var orig = meta.superMeta;
-                meta.superMeta = {f: f, pos: 0, name: name};
-                ret = f.apply(this, arguments);
-                meta.superMeta = orig;
-                return ret;
-            };
-            wrapper._f = f;
-            return wrapper;
+            if (f.toString().match(SUPER_REGEXP)) {
+                var wrapper = function wrapper() {
+                    var ret, meta = this.__meta || {};
+                    var orig = meta.superMeta;
+                    meta.superMeta = {f: f, pos: 0, name: name};
+                    switch (arguments.length) {
+                    case 0:
+                        ret = f.call(this);
+                        break;
+                    case 1:
+                        ret = f.call(this, arguments[0]);
+                        break;
+                    case 2:
+                        ret = f.call(this, arguments[0], arguments[1]);
+                        break;
+
+                    case 3:
+                        ret = f.call(this, arguments[0], arguments[1], arguments[2]);
+                        break;
+                    default:
+                        ret = f.apply(this, arguments);
+                    }
+                    meta.superMeta = orig;
+                    return ret;
+                };
+                wrapper._f = f;
+                return wrapper;
+            } else {
+                f._f = f;
+                return f;
+            }
         }
 
         function defineMixinProps(child, proto) {
