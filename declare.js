d68867f69cc6c1af17ee3cbc76138e28cc21fedd
Doug Martin <doug@dougamartin.com>
2013-06-17 17:27:49 -0500

    v0.0.7
    * Changed Constructor to not use apply unless needed

--- declare.js
+++ declare.js
@@ -854,7 +854,22 @@
 
         function declare(sup, proto) {
             function declared() {
-                this.constructor.apply(this, arguments);
+                switch (arguments.length) {
+                case 0:
+                    this.constructor.call(this);
+                    break;
+                case 1:
+                    this.constructor.call(this, arguments[0]);
+                    break;
+                case 2:
+                    this.constructor.call(this, arguments[0], arguments[1]);
+                    break;
+                case 3:
+                    this.constructor.call(this, arguments[0], arguments[1], arguments[2]);
+                    break;
+                default:
+                    this.constructor.apply(this, arguments);
+                }
             }
 
             __declare(declared, sup, proto);
